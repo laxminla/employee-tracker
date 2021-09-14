edeed015bed6b5063baf121a1019339c08b72632
Laxmi Sharma <laxminla@yahoo.com>
2021-09-14 14:19:34 -0700

    added schema and seeds sql

---
+++ seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department(department_name)
+VALUES ('Management'),
+('Sales'),
+('Warehouse'),
+('Human Resources'),
+('Quality Control');
+
+INSERT INTO role(title)
+VALUES('Regional Manager'),
+('Sales Rep'),
+('HR Rep');
+
+INSERT INTO employee(first_name, last_name)
+VALUES('Laxmi', 'Sharma'),
+('Diyana', 'Das'),
+('Maya', 'Rogar')
