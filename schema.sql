2e1341ec4ac52cfbbca1a18c5f6001254399bac6
Laxmi Sharma <laxminla@yahoo.com>
2021-09-15 11:17:31 -0700

    updated start function

--- schema.sql
+++ schema.sql
@@ -4,23 +4,23 @@ USE employee_db;
 
 CREATE TABLE department(
 id INT AUTO_INCREMENT PRIMARY KEY,
-department_name VARCHAR(30)
+department_name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
 id INT AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(30),
-salary DECIMAL(8,2),
+title VARCHAR(50),
+salary DECIMAL,
 department_id INT,
-FOREIGN KEY(department_id) REFERENCES departments(id)
+FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
-id INT AUTO_INCREMENT PRIMARY KEY,
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT,
 manager_id INT,
-FOREIGN KEY(role_id) REFERENCES roles(id),
+FOREIGN KEY(role_id) REFERENCES role(id),
 FOREIGN KEY(manager_id) REFERENCES employees(id)
 );
--- server.js
+++ server.js
@@ -1,1 +1,80 @@
+// Dependencies
+const express = require('express')
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const consoleTable = require('console.table');
+const Connection = require('mysql2/typings/mysql/lib/Connection');
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Diwaz',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+  // connection to mysql server and database
+  Connection.connect(function(err){
+    if(err) throw err;
+  console.log('SQL connected')
+  Start();
+  });
+
+
+
+function start(){
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "start",
+      choices: ["View", "Add", "Update", "Exit"],
+      message: "We have information on employees, departments, and employee roles. What would you like to do?",
+    }
+  ]).then (function(res){
+    switch(res.start){
+      case 'View':
+      View();
+      break; 
+      
+      case 'Add':
+        Add();
+        break; 
+
+        case 'Update':
+          updateEmployee();
+          break; 
+
+          case 'Exit':
+            console.log('-------------');
+            console.log('All done');
+            console.log('--------------');
+            break; 
+        default:
+          console.log('default')
+    }
+  })
+};
+
+// view function
+
+
+
+
+
+
+
+
